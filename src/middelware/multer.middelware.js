f411cbfed54ec8e9c2fcdbe25ad070cc19572aa8
Akshat Panchal <akshatpanchal001@gmail.com>
2025-01-02 19:55:08 +0530

    revert changes

--- src/middelware/multer.middelware.js
+++ src/middelware/multer.middelware.js
@@ -1,12 +1,4 @@
 import multer from "multer";
-import fs from 'fs';
-import path from 'path';
-
-const tempDir = path.join(__dirname, 'public', 'temp');
-
-if (!fs.existsSync(tempDir)) {
-    fs.mkdirSync(tempDir, { recursive: true });
-}
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
