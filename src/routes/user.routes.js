db17c380a3bc887d6c56c6c680cf802ba37a59cc
Akshat Panchal <akshatpanchal001@gmail.com>
2025-02-28 10:58:25 +0530

    refactor: update user routes to include admin logout functionality

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,16 +1,23 @@
 import { Router } from "express";
-import { adminLogin, createUser, getCurrentUser,  userLogin, userLogout} from "../controller/user.controller.js";
-import { verifyAdmin, verifyJwt} from '../middelware/auth.middelware.js' 
+import {
+  adminLogin,
+  adminLogout,
+  createUser,
+  getCurrentUser,
+  userLogin,
+  userLogout,
+} from "../controller/user.controller.js";
+import { verifyAdmin, verifyJwt } from "../middelware/auth.middelware.js";
 
-const router = Router()
+const router = Router();
 
-router.post('/register-user',createUser)
-router.post('/login',userLogin)
-router.post('/login-admin',adminLogin)
+router.post("/register-user", createUser);
+router.post("/login", userLogin);
+router.post("/login-admin", adminLogin);
 
 //secured routes
-router.post('/logout',verifyJwt,userLogout)
-router.post('/logout-admin',verifyAdmin,adminLogin)
-router.get('/get-current-user',verifyJwt,getCurrentUser)
+router.post("/logout", verifyJwt, userLogout);
+router.post("/logout-admin", verifyAdmin, adminLogout);
+router.get("/get-current-user", verifyJwt, getCurrentUser);
 
-export default router
+export default router;
